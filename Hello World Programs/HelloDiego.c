d8d0852e5ecbacd05a27fb129c5bd68918e1946c
CodeBreaker518 <49607711+CodeBreaker518@users.noreply.github.com>
2020-10-27 09:50:15 -0600

    HelloDiego.c
    I added my Hello World program on C lenguage!
    Happy Hacktoberfest

---
+++ Hello World Programs/HelloDiego.c
@@ -0,0 +1,5 @@
+#include <stdio.h>                              //add basic library
+int main() {                                    //open main function
+   printf("Hello World, my name is Diego!");   // printf() displays the string inside quotation
+   return 0;                                    //return a 0 value
+}                                               //close main function
