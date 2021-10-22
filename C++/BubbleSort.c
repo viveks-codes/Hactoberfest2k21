512d205b31acdc774efa3541dc6cc27d77670943
Rutvi <82574816+rutvi02@users.noreply.github.com>
2021-10-22 13:57:07 +0530

    Created a Bubble sort program

---
+++ C++/BubbleSort.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+{
+	int arr[] = {34,45,56,23,7,1,79};
+	int t,i,j,k;
+	int n=7;
+	
+	
+	
+	for(i=0; i<n; i++)
+	{
+		for(j=0; j<n; j++)
+		{
+			if (arr[j]>arr[j+1])
+			{
+				t= arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=t;
+				
+			}
+		}
+	}
+	
+	printf("\n Array after sorting:");
+	
+	for(k=0; k<n; k++)
+	{
+		printf("\n %d",arr[k]);
+	}
+	
+}
