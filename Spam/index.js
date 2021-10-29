3b60f38dbcb7700238faf84a5d9b0ff11c45a001
Sarthak gupta <53075899+sarthakgdeveloper@users.noreply.github.com>
2021-10-29 13:21:48 +0530

    Update index.js

--- Spam/index.js
+++ Spam/index.js
@@ -1,1 +1,7 @@
-console.log('tshirt milegi');
+function showFormattedDate() {
+  var d = new Date();
+  var curr_date = d.getDate();
+  var curr_month = d.getMonth() + 1; //months are zero based
+  var curr_year = d.getFullYear();
+  document.write(curr_date + "-" + curr_month + "-" + curr_year);
+}
